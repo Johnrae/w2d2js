afcdc10d0a78379d71761ba802f4ef654c6ed5cb
John Rae <john.rae23@gmail.com>
2015-09-29 13:49:09 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// // greeting function
+// var greeting = function (name){
+//   return 'hello ' + name;
+// };
+
+// // grab my elements
+// var nameInput = document.querySelector ('#your-name')
+// var greetBtn = document.querySelector('#greetme')
+// var helloElem = document.querySelector ('#greeting')
+
+// // variable declarations
+// var fullName, greetingTxt;
+
+// // Function to perform the greeting
+// var greetMe = function() {
+//   fullName = nameInput.value;
+//   greetingTxt = greeting(fullName);
+//   helloElem.textContent = greetingTxt;
+// };
+
+// // Button click
+// greetBtn.addEventListener('click',greetMe);
